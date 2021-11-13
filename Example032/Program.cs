cf98fc1c363ed61d2ac6dcb12692b3f282a4c817
SaiPhone <saiphone94@yandex.ru>
2021-11-13 18:54:58 +0300

    Example 032

---
+++ Example032/Program.cs
@@ -0,0 +1,15 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+
+int[] Arr(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write(array[i] + " ");
+
+    }
+    return array;
+}
+Arr(10);
