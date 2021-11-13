8089c629a5e896c8ea7ee284fb9ab55bdc190be4
SaiPhone <saiphone94@yandex.ru>
2021-11-13 18:37:02 +0300

    Example 028

---
+++ Example028/Program.cs
@@ -0,0 +1,14 @@
+﻿// 28. Подсчитать сумму цифр в числе
+int SummaChisel(int number)
+{
+// int number = 2134;
+int summa = 0;
+while (number > 0)
+{
+    summa = summa + number % 10;
+    number = number /10;
+}
+Console.WriteLine(summa);
+return summa;
+}
+SummaChisel(2341);
