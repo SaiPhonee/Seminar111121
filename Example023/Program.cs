65174059cc59d8f51bfd6b9515894829213edd7e
SaiPhone <saiphone94@yandex.ru>
2021-11-13 15:58:23 +0300

    Example023

---
+++ Example023/Program.cs
@@ -0,0 +1,8 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+int Power(int n)
+{
+    for (int i = 1; i <= n; i++)
+    Console.WriteLine($"{i} x {i} = {i * i}");
+    return n;
+}
+Power(7);
