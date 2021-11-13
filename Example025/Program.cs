dcd561a906dc89dc2708138c21a520600c451cfd
SaiPhone <saiphone94@yandex.ru>
2021-11-13 16:11:23 +0300

    Example 025

---
+++ Example025/Program.cs
@@ -0,0 +1,13 @@
+﻿// 25. Найти сумму чисел от 1 до А
+int summa = 0;
+int Sum(int a)
+{
+    for (int i = 0; i <= a; i++)
+    {
+        // int summa = 0;
+        summa = summa + i;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {a} = {summa}");
+    return summa;
+}
+Sum(5);
