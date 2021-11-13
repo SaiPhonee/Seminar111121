3a46acc71899f89c4ff2df1c2067d611d657bb0c
SaiPhone <saiphone94@yandex.ru>
2021-11-13 16:14:57 +0300

    Example 026

--- Example025/Program.cs
+++ Example025/Program.cs
@@ -1,13 +1,20 @@
 ﻿// 25. Найти сумму чисел от 1 до А
-int summa = 0;
 int Sum(int a)
 {
+    int summa = 0;
     for (int i = 0; i <= a; i++)
     {
         // int summa = 0;
         summa = summa + i;
     }
-    Console.WriteLine($"Сумма чисел от 1 до {a} = {summa}");
+    // Console.WriteLine($"Сумма чисел от 1 до {a} = {summa}");
     return summa;
 }
-Sum(5);
+if (6 == Sum(3))
+{
+    Console.WriteLine("Работает корректно");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
---
+++ Example026/Program.cs
@@ -0,0 +1,20 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+int toPower(int number, int degree)
+{
+    int result = 1;
+    for (int i = 1; i <= degree; i++)
+    {
+        result = result * number;
+    }
+    return result;
+}
+
+if (8 == toPower(2, 3))
+{
+    Console.WriteLine("Работает корректно");
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
