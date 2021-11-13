4699bf39921f9d18b2733aa8e6c80cd8c64e35ab
SaiPhone <saiphone94@yandex.ru>
2021-11-13 18:53:22 +0300

    Example 031

---
+++ Example031/Program.cs
@@ -0,0 +1,13 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+int[] Array(int size)
+{
+    int[] array = new int[8];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+Array(8);
