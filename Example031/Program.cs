2fed33f371247015ec4b109c9573f4454be8f606
SaiPhone <saiphone94@yandex.ru>
2021-11-13 18:54:46 +0300

    Example 031upd

--- Example031/Program.cs
+++ Example031/Program.cs
@@ -4,7 +4,6 @@ int[] Array(int size)
     int[] array = new int[8];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(1, 100);
         Console.Write(array[i] + " ");
     }
     return array;
