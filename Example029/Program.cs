4467efee92466ea203e8024fb0c099c7c78d5143
SaiPhone <saiphone94@yandex.ru>
2021-11-13 18:46:04 +0300

    Example 029

---
+++ Example029/Program.cs
@@ -0,0 +1,13 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+int Multiplication(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result = result * i;
+    }
+    Console.WriteLine($"Произведение чисел от 1 до {n} = {result}");
+    return result;
+}
+
+Multiplication(5);
